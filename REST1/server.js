8634bc35beee33bf77b0b3bc99209870c9098f94
Joni Laatikainen <jonilaatikaine@gmail.com>
2022-10-07 20:58:54 +0300

    REST1 done

--- REST1/server.js
+++ REST1/server.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const filesync = require("fs");
 
 const port = 3000;
 
+// Datan hakemiseen
 app.get("/", (req, res) => {
-  const data = filesync.readFileSync("./sanakirja.txt", {
-    encoding: "utf8",
-    flag: "r",
-  });
+  const data = filesync.readFileSync("./sanakirja.txt");
   res.send(data);
 });
 
+// Datan lisäämiseen
 app.post("/", (req, res) => {
-  const data = String(req.body);
-  filesync.writeFileSync("./sanakirja.txt", data, {
-    encoding: "utf8",
-    flag: "a+",
-    mode: 0o666,
-  });
+  const data = req.body;
+  filesync.appendFileSync(
+    "./sanakirja.txt",
+    `${data.sanat[0].finnish} ${data.sanat[0].english}\n`
+  );
+  res.json(data.sanat[0]);
 });
 
 app.listen(port, () => {
