97bc86205b6a406c64d298022719dc419c9fc263
Joni Laatikainen <jonilaatikaine@gmail.com>
2022-11-01 13:25:49 +0200

    committi

--- REST1/server.js
+++ REST1/server.js
@@ -1,14 +1,45 @@
 const express = require("express");
+
 const app = express();
+
 app.use(express.json());
+
 const filesync = require("fs");
 
 const port = 3000;
 
+let dictionaryLines = [];
+let dictionaryWords = [];
+
+const refreshDictionaryLines = () => {
+  dictionaryLines = filesync.readFile("./sanakirja.txt", (err) => {
+    if (err) throw err;
+  });
+  if (dictionaryLines.length > 0) {
+    dictionaryLines = dictionaryLines.split("\n");
+  }
+};
+
+const refreshDictionaryWords = () => {
+  refreshDictionaryLines();
+  if (dictionaryLines.length > 0) {
+    dictionarywords = dictionaryLines.map((line) => {
+      const sanat = line.split(" ");
+
+      const sana = { finnish: sanat[0], english: sanat[1] };
+      return sana;
+    });
+  } else {
+    console.log("Sanoja ei lisätty");
+  }
+};
+
 // Datan hakemiseen
 app.get("/", (req, res) => {
-  const data = filesync.readFile("./sanakirja.txt");
-  res.send(data);
+  const sana = req.body.sana;
+
+  console.log(sana);
+  res.send(sana);
 });
 
 // Datan lisäämiseen
@@ -27,3 +58,5 @@ app.post("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Online at port ${port}`);
 });
+
+refreshDictionaryWords();
